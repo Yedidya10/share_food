871273974ffe8377848e8ba5d53255560d13e026
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-06-01 13:52:10 +0300

    fix: update redirect URL to use NEXT_PUBLIC_URL for forwarded host

--- src/app/[locale]/auth/callback/route.ts
+++ src/app/[locale]/auth/callback/route.ts
@@ -22,7 +22,8 @@ export async function GET(request: Request) {
         // we can be sure that there is no load balancer in between, so no need to watch for X-Forwarded-Host
         return NextResponse.redirect(`${origin}${next}`);
       } else if (forwardedHost) {
-        return NextResponse.redirect(`https://${forwardedHost}${next}`);
+        const baseUrl = process.env.NEXT_PUBLIC_URL;
+        return NextResponse.redirect(`https://${baseUrl}${next}`);
       } else {
         return NextResponse.redirect(`${origin}${next}`);
       }

03d1768d88241105692b56079068f740e82c9b64
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-06-03 01:45:30 +0300

    fix: update OAuth redirect URL to use HTTPS for development environment

--- src/lib/supabase/actions.ts
+++ src/lib/supabase/actions.ts
@@ -66,7 +66,7 @@ export async function signInWithOAuth(provider: Provider) {
     options: {
       redirectTo: `${
         process.env.NODE_ENV === "development"
-          ? "http://localhost:3000"
+          ? "https://localhost:3000"
           : process.env.NEXT_PUBLIC_URL
       }/auth/callback`,
     },

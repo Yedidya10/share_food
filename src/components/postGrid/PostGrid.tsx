3e0db628874f6e2e9f33fb7fd8648185f5caa9f8
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-04 22:53:51 +0300

    feat: refactor PostGrid component to accept userId and items as props, enhancing data handling and rendering logic

--- src/components/postGrid/PostGrid.tsx
+++ src/components/postGrid/PostGrid.tsx
@@ -1,10 +1,7 @@
 "use client";
 
 import { Card, CardContent } from "@/components/ui/card";
-import { useEffect, useState } from "react";
 import Image from "next/image";
-import { Skeleton } from "@/components/ui/skeleton";
-import { createClient } from "@/lib/supabase/client";
 import {
   Dialog,
   DialogTrigger,
@@ -24,220 +21,193 @@ import Link from "next/link";
 import { FaWhatsapp } from "react-icons/fa";
 import { Button } from "../ui/button";
 import { Tooltip } from "../ui/tooltip";
-import { StartChatButton } from "../startChatButton/StartChatButton";
+import StartChatButton from "../startChatButton/StartChatButton";
 
-type Post = {
+type Item = {
   user_id: string;
   id: string;
   title: string;
   description: string;
   images: string[];
-  street_name?: string;
-  street_number?: string;
-  city?: string;
+  full_name: string;
+  street_name: string;
+  street_number: string;
+  city: string;
   postal_code?: string;
-  country?: string;
+  country: string;
   phone_number?: string;
   is_have_whatsapp?: boolean;
   email?: string;
+  status: "pending" | "published" | "draft";
   created_at: string;
 };
 
-export default function PostGrid() {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [currentUserId, setCurrentUserId] = useState<string | null>(null);
-  const [loading, setLoading] = useState(true);
-
-  const supabase = createClient();
-
-  // Fetch current user id on mount
-  useEffect(() => {
-    async function fetchCurrentUser() {
-      const { data: sessionData } = await supabase.auth.getSession();
-      setCurrentUserId(sessionData?.session?.user.id ?? null);
-    }
-    fetchCurrentUser();
-  }, [supabase]);
-
-  useEffect(() => {
-    async function fetchPosts() {
-      const { data, error } = await supabase
-        .from("items")
-        .select("*")
-        .order("created_at", { ascending: false });
-
-      if (error) {
-        console.error("Failed to fetch posts", error);
-      } else {
-        setPosts(data as Post[]);
-      }
-
-      setLoading(false);
-    }
-
-    fetchPosts();
-  }, [supabase]);
-
-  if (loading) {
-    return (
-      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4'>
-        {Array.from({ length: 10 }).map((_, i) => (
-          <Skeleton key={i} className='h-64 w-full rounded-2xl' />
-        ))}
-      </div>
-    );
-  }
-
+export default function PostGrid({
+  userId,
+  items,
+}: {
+  userId: string | undefined;
+  items: Item[];
+}) {
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 p-4'>
-      {posts.map((post) =>
-        // If the post owner is the current user, hide the Card
-        post.user_id === currentUserId ? null : (
-          <Card
-            key={post.id}
-            className='rounded-2xl shadow-lg hover:shadow-xl transition-shadow p-0 overflow-hidden'
-          >
-            <div className='rounded-2xl overflow-hidden'>
-              <Carousel
-                className='w-full max-w-xs m-auto p-0 rounded'
-                opts={{
-                  loop: true,
-                }}
-              >
-                <CarouselContent>
-                  {Array.from(post.images).map((image, index) => (
-                    <CarouselItem key={index}>
-                      <div className='relative h-48 overflow-hidden border-b'>
-                        <Image
-                          src={image}
-                          alt={`${post.title} blurred background`}
-                          fill
-                          className='object-cover blur-sm scale-110 opacity-70'
-                          aria-hidden='true'
-                        />
-                        <Image
-                          src={image}
-                          alt={post.title}
-                          fill
-                          className='object-contain z-10'
-                        />
-                      </div>
-                    </CarouselItem>
-                  ))}
-                </CarouselContent>
-                <CarouselPrevious
-                  variant='outline'
-                  className='absolute left-2'
-                />
-                <CarouselNext variant='outline' className='absolute right-2' />
-              </Carousel>
-            </div>
-            <CardContent className='p-4 space-y-2'>
-              <h3 className='text-xl font-semibold'>{post.title}</h3>
-              <p className='text-sm text-muted-foreground line-clamp-3'>
-                {post.description}
-              </p>
-              <p className='text-xs text-right text-muted-foreground'>
-                {new Date(post.created_at).toLocaleDateString()}
-              </p>
-              <Dialog>
-                <DialogTrigger asChild>
-                  <Button
+      {items.map(
+        (item) =>
+          item.user_id !== userId &&
+          item.status === "published" && (
+            <Card
+              key={item.id}
+              className='rounded-2xl shadow-lg hover:shadow-xl transition-shadow p-0 overflow-hidden'
+            >
+              <div className='rounded-2xl overflow-hidden'>
+                <Carousel
+                  className='w-full max-w-xs m-auto p-0 rounded'
+                  opts={{
+                    loop: true,
+                  }}
+                >
+                  <CarouselContent>
+                    {Array.from(item.images).map((image, index) => (
+                      <CarouselItem key={index}>
+                        <div className='relative h-48 overflow-hidden border-b'>
+                          <Image
+                            src={image}
+                            alt={`${item.title} blurred background`}
+                            fill
+                            className='object-cover blur-sm scale-110 opacity-70'
+                            aria-hidden='true'
+                          />
+                          <Image
+                            src={image}
+                            alt={item.title}
+                            fill
+                            className='object-contain z-10'
+                          />
+                        </div>
+                      </CarouselItem>
+                    ))}
+                  </CarouselContent>
+                  <CarouselPrevious
                     variant='outline'
-                    className='w-full mt-2'
-                    onClick={() => {
-                      const dialog = document.querySelector(
-                        `#dialog-${post.id}`
-                      ) as HTMLDialogElement;
-                      dialog?.showModal();
-                    }}
-                  >
-                    הצג פרטים
-                  </Button>
-                </DialogTrigger>
-                <DialogContent className='h-120 overflow-y-auto max-w-3xl'>
-                  <DialogHeader>
-                    <DialogTitle>{post.title}</DialogTitle>
-                    <DialogDescription>
-                      פורסם בתאריך{" "}
-                      {new Date(post.created_at).toLocaleDateString()}
-                    </DialogDescription>
-                  </DialogHeader>
-                  <div className='space-y-6'>
-                    <Carousel className='w-full' opts={{ loop: true }}>
-                      <CarouselContent>
-                        {post.images.map((image, index) => (
-                          <CarouselItem key={index}>
-                            <div className='relative h-64 rounded overflow-hidden'>
-                              <Image
-                                src={image}
-                                alt={`${post.title} image ${index + 1}`}
-                                fill
-                                className='object-cover'
-                              />
+                    className='absolute left-2'
+                  />
+                  <CarouselNext
+                    variant='outline'
+                    className='absolute right-2'
+                  />
+                </Carousel>
+              </div>
+              <CardContent className='p-4 space-y-2'>
+                <h3 className='text-xl font-semibold'>{item.title}</h3>
+                <p className='text-sm text-muted-foreground line-clamp-3'>
+                  {item.description}
+                </p>
+                <p className='text-xs text-right text-muted-foreground'>
+                  {new Date(item.created_at).toLocaleDateString()}
+                </p>
+                <Dialog>
+                  <DialogTrigger asChild>
+                    <Button
+                      variant='outline'
+                      className='w-full mt-2'
+                      onClick={() => {
+                        const dialog = document.querySelector(
+                          `#dialog-${item.id}`
+                        ) as HTMLDialogElement;
+                        dialog?.showModal();
+                      }}
+                    >
+                      הצג פרטים
+                    </Button>
+                  </DialogTrigger>
+                  <DialogContent className='h-120 overflow-y-auto max-w-3xl'>
+                    <DialogHeader>
+                      <DialogTitle>{item.title}</DialogTitle>
+                      <DialogDescription>
+                        פורסם בתאריך{" "}
+                        {new Date(item.created_at).toLocaleDateString()}
+                      </DialogDescription>
+                    </DialogHeader>
+                    <div className='space-y-6'>
+                      <Carousel className='w-full' opts={{ loop: true }}>
+                        <CarouselContent>
+                          {item.images.map((image, index) => (
+                            <CarouselItem key={index}>
+                              <div className='relative h-64 rounded overflow-hidden'>
+                                <Image
+                                  src={image}
+                                  alt={`${item.title} image ${index + 1}`}
+                                  fill
+                                  className='object-cover'
+                                />
+                              </div>
+                            </CarouselItem>
+                          ))}
+                        </CarouselContent>
+                        <CarouselPrevious
+                          variant='outline'
+                          className='absolute left-2 top-1/2 -translate-y-1/2'
+                        />
+                        <CarouselNext
+                          variant='outline'
+                          className='absolute right-2 top-1/2 -translate-y-1/2'
+                        />
+                      </Carousel>
+                      <div>
+                        <h4 className='text-lg font-semibold mb-1'>תיאור:</h4>
+                        <p className='text-base text-muted-foreground'>
+                          {item.description}
+                        </p>
+                      </div>
+                      <div>
+                        <h4 className='text-lg font-semibold mb-1'>
+                          כתובת איסוף:
+                        </h4>
+                        <div className='grid grid-cols-1 sm:grid-cols-3 gap-2 text-base'>
+                          <p>{`${item.street_name} ${item.street_number}`}</p>
+                          <p>{item.city}</p>
+                          <p>{item.country}</p>
+                        </div>
+                      </div>
+                      <div>
+                        <h4 className='text-lg font-semibold mb-1'>
+                          פרטי קשר:
+                        </h4>
+                        <div className='flex flex-col sm:flex-row items-start sm:items-center justify-between gap-2'>
+                          <p className='text-base'>{item.full_name}</p>
+                        </div>
+                        <div className='grid grid-cols-1 sm:grid-cols-1 gap-2 text-base'>
+                          <StartChatButton targetUserId={item.user_id} />
+                          <p>{item.email}</p>
+                          {item.phone_number && (
+                            <div className='flex align-center gap-2'>
+                              <p>טלפון: </p>
+                              <p>{item.phone_number}</p>
+                              {item.is_have_whatsapp && (
+                                <Tooltip>
+                                  <Link
+                                    href={`https://wa.me/${item.phone_number.replace(
+                                      /[^0-9]/g,
+                                      ""
+                                    )}`}
+                                    target='_blank'
+                                    rel='noopener noreferrer'
+                                    className='inline-flex items-center text-green-500 hover:text-green-700'
+                                  >
+                                    <FaWhatsapp size={20} />
+                                  </Link>
+                                </Tooltip>
+                              )}
                             </div>
-                          </CarouselItem>
-                        ))}
-                      </CarouselContent>
-                      <CarouselPrevious
-                        variant='outline'
-                        className='absolute left-2 top-1/2 -translate-y-1/2'
-                      />
-                      <CarouselNext
-                        variant='outline'
-                        className='absolute right-2 top-1/2 -translate-y-1/2'
-                      />
-                    </Carousel>
-                    <div>
-                      <h4 className='text-lg font-semibold mb-1'>תיאור:</h4>
-                      <p className='text-base text-muted-foreground'>
-                        {post.description}
-                      </p>
-                    </div>
-                    <div>
-                      <h4 className='text-lg font-semibold mb-1'>
-                        כתובת איסוף:
-                      </h4>
-                      <div className='grid grid-cols-1 sm:grid-cols-3 gap-2 text-base'>
-                        <p>{`${post.street_name} ${post.street_number}`}</p>
-                        <p>{post.city}</p>
-                        <p>{post.country}</p>
+                          )}
+                        </div>
                       </div>
                     </div>
-                    <div>
-                      <h4 className='text-lg font-semibold mb-1'>פרטי קשר:</h4>
-                      <div className='grid grid-cols-1 sm:grid-cols-1 gap-2 text-base'>
-                        <StartChatButton targetUserId={post.user_id} />
-                        <p>{post.email}</p>
-                        {post.phone_number && (
-                          <div className='flex align-center gap-2'>
-                            <p>טלפון: </p>
-                            <p>{post.phone_number}</p>
-                            {post.is_have_whatsapp && (
-                              <Tooltip>
-                                <Link
-                                  href={`https://wa.me/${post.phone_number.replace(
-                                    /[^0-9]/g,
-                                    ""
-                                  )}`}
-                                  target='_blank'
-                                  rel='noopener noreferrer'
-                                  className='inline-flex items-center text-green-500 hover:text-green-700'
-                                >
-                                  <FaWhatsapp size={20} />
-                                </Link>
-                              </Tooltip>
-                            )}
-                          </div>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                </DialogContent>
-              </Dialog>
-            </CardContent>
-          </Card>
-        )
+                  </DialogContent>
+                </Dialog>
+              </CardContent>
+            </Card>
+          )
       )}
     </div>
   );

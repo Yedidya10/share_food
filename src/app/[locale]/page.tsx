932084ba5499209d8533824db9b68f544379fe4d
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-04 22:54:23 +0300

    feat: fetch user and items data in Home component, passing userId and items to PostGrid

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,4 +1,25 @@
 import PostGrid from "@/components/postGrid/PostGrid";
+import { createClient } from "@/lib/supabase/server";
+
 export default async function Home() {
-  return <PostGrid />;
+  const supabase = await createClient();
+  const { data: userData } = await supabase.auth.getUser();
+
+  const { data: itemsData, error: itemsError } = await supabase
+    .from("items")
+    .select("*")
+    .order("created_at", { ascending: false });
+
+  if (itemsError) {
+    console.error("Error fetching items:", itemsError);
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <p className='text-red-500'>
+          Error fetching items. Please try again later.
+        </p>
+      </div>
+    );
+  }
+
+  return <PostGrid userId={userData.user?.id} items={itemsData || []} />;
 }

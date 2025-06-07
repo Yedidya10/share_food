499a24c754e82f4921993ca1a289e6f1e65e7931
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-07 15:49:06 +0300

    feat: add ChatButton component with tooltip and link to chat

---
+++ src/components/chatButton/ChatButton.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { MessageCircle } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+export default function ChatButton() {
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant='outline' className='h-10'>
+          <Link href='/chat' className='flex items-center gap-2'>
+            <MessageCircle />
+          </Link>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>Chat</TooltipContent>
+    </Tooltip>
+  );
+}

bb60d185bd1e111e34198f26610b63c00d83fecf
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-07 12:47:38 +0300

    refactor: simplify OAuth sign-in URL handling and remove unnecessary logging

--- src/lib/supabase/actions.ts
+++ src/lib/supabase/actions.ts
@@ -58,34 +58,27 @@ export async function logout() {
   redirect("/");
 }
 
+const getURL = () => {
+  let url =
+    process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+    process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+    "https://localhost:3000/";
+  // Make sure to include `https://` when not localhost.
+  url = url.startsWith("http") ? url : `https://${url}`;
+  // Make sure to include a trailing `/`.
+  url = url.endsWith("/") ? url : `${url}/`;
+  return url;
+};
+
 export async function signInWithOAuth(provider: Provider) {
   const supabase = await createClient();
 
-  const { data, error } = await supabase.auth.signInWithOAuth({
+  const { error } = await supabase.auth.signInWithOAuth({
     provider,
     options: {
-      redirectTo: `${
-        process.env.NODE_ENV === "development"
-          ? "https://localhost:3000"
-          : process.env.NEXT_PUBLIC_URL
-      }/auth/callback`,
+      redirectTo: getURL(),
     },
   });
-
-  console.log("NODE_ENV:", process.env.NODE_ENV);
-  console.log("NEXT_PUBLIC_URL:", process.env.NEXT_PUBLIC_URL);
-  console.log(
-    "Redirecting to:",
-    `${
-      process.env.NODE_ENV === "development"
-        ? "https://localhost:3000"
-        : process.env.NEXT_PUBLIC_URL
-    }/auth/callback`
-  );
-
-  if (data.url) {
-    redirect(data.url); // use the redirect API for your server framework
-  }
 
   if (error) {
     redirect("/error");

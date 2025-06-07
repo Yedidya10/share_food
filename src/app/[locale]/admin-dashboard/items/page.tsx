a88e566127d43266b92a4c00b9f0694d8c73fca5
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-07 03:45:06 +0300

    feat: implement ItemsPage and ItemsList components for managing user items

---
+++ src/app/[locale]/admin-dashboard/items/page.tsx
@@ -0,0 +1,73 @@
+import ItemsList from "@/components/itemsList/ItemsList";
+import { createClient } from "@/lib/supabase/server";
+
+export default async function ItemsPage() {
+  try {
+    const supabase = await createClient();
+
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+
+    if (userError) {
+      return (
+        <div className='flex items-center justify-center h-screen'>
+          <p className='text-gray-500'>
+            You need to be logged in to view this page.
+          </p>
+        </div>
+      );
+    }
+
+    const { data: usersData, error: usersError } = await supabase
+      .from("users")
+      .select("id, role")
+      .eq("id", userData.user?.id)
+      .single();
+
+    if (usersError) {
+      throw new Error("Error fetching user data: " + usersError.message);
+    }
+
+    // Check if user is authenticated admin
+    if (usersData?.role !== "admin") {
+      return (
+        <div className='flex items-center justify-center h-screen'>
+          <p className='text-gray-500'>
+            You do not have permission to view this page.
+          </p>
+        </div>
+      );
+    }
+
+    // Fetch user's items from the database
+    const { data: items, error: itemsError } = await supabase
+      .from("items")
+      .select("*");
+    if (itemsError) {
+      return (
+        <div className='flex items-center justify-center h-screen'>
+          <p className='text-red-500'>
+            Error fetching items. Please try again later.
+          </p>
+        </div>
+      );
+    }
+    if (!items || items.length === 0) {
+      return (
+        <div className='flex items-center justify-center h-screen'>
+          <p className='text-gray-500'>No items found.</p>
+        </div>
+      );
+    }
+
+    return <ItemsList items={items} />;
+  } catch (error) {
+    console.error(error);
+    return (
+      <div className='flex items-center justify-center h-screen'>
+        <p className='text-red-500'>
+          An unexpected error occurred. Please try again later.
+        </p>
+      </div>
+    );
+  }
+}
---
+++ src/components/itemsList/ItemsList.tsx
@@ -0,0 +1,239 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Badge } from "@/components/ui/badge";
+import {
+  BookCheck,
+  BookDashed,
+  CircleCheck,
+  Hammer,
+  Loader,
+  Trash2,
+} from "lucide-react";
+import EditItemForm from "@/components/editItemForm/EditItemForm";
+import { useTranslations } from "next-intl";
+import { createClient } from "@/lib/supabase/client";
+import Image from "next/image";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { useState } from "react";
+
+type itemsType = {
+  id: string;
+  title: string;
+  description: string;
+  images: File[];
+  street_name: string;
+  street_number: string;
+  city: string;
+  postal_code: string;
+  country: string;
+  phone_number: string;
+  is_have_whatsapp: boolean;
+  email: string;
+  status: "published" | "pending" | "draft";
+  user_id: string;
+  created_at: string;
+};
+
+export default function ItemsList({ items }: { items: itemsType[] }) {
+  const [itemsData, setItemsData] = useState<Array<itemsType>>(items);
+
+  const tPostItemForm = useTranslations("form.postItem");
+  const tEditItemForm = useTranslations("form.editItem");
+  const tGenericForm = useTranslations("form.generic");
+  const tCountries = useTranslations("countries");
+
+  const deleteItem = async (id: string) => {
+    const supabase = createClient();
+    const { error } = await supabase.from("items").delete().eq("id", id);
+
+    if (error) {
+      console.error("Error deleting item:", error);
+      return;
+    }
+
+    // Trigger a re-fetch of the items or update the state
+    // This could be done by lifting the state up to a parent component
+    setItemsData((prevItems) => prevItems.filter((item) => item.id !== id));
+  };
+
+  return (
+    <div className='space-y-4'>
+      {itemsData.map((item) => (
+        <div
+          key={item.id}
+          className='border rounded p-4 flex flex-col md:flex-row md:items-center justify-between'
+        >
+          <div className='grid grid-cols-1 md:grid-cols-3 gap-4 items-start'>
+            <div className='flex items-center gap-4 md:gap-6'>
+              <div className='flex flex-col md:flex-row md:items-center space-y-2 md:space-y-0 md:space-x-4 relative w-[50px] h-[50px] md:w-[60px] md:h-[60px]'>
+                <Image
+                  src={
+                    typeof item.images[0] === "string"
+                      ? item.images[0]
+                      : "/placeholder-image.png"
+                  }
+                  alt={item.title}
+                  className='object-cover rounded-md'
+                  fill
+                />
+              </div>
+              <div className='flex flex-col space-y-2'>
+                <h3 className='font-semibold text-lg'>{item.title}</h3>
+                <Badge
+                  variant={
+                    item.status === "published" ? "secondary" : "outline"
+                  }
+                >
+                  {item.status === "published" ? (
+                    <div className='flex items-center gap-1'>
+                      <CircleCheck
+                        className='inline text-green-500'
+                        size={14}
+                      />
+                      <span>{tPostItemForm("status.published")}</span>
+                    </div>
+                  ) : item.status === "pending" ? (
+                    <div className='flex items-center gap-1'>
+                      <Loader className='inline' size={14} color='#FFA500' />
+                      <span>{tPostItemForm("status.pending")}</span>
+                    </div>
+                  ) : (
+                    <div className='flex items-center gap-1'>
+                      <Hammer className='inline' size={14} color='#808080' />
+                      <span>{tPostItemForm("status.draft")}</span>
+                    </div>
+                  )}
+                </Badge>
+              </div>
+            </div>
+            <div className='text-sm text-gray-500 mr-8'>
+              <p>{item.description}</p>
+              <p>
+                {item.phone_number && (
+                  <>
+                    <span>{tPostItemForm("phoneNumber")}: </span>
+                    <span>{item.phone_number}</span>
+                  </>
+                )}
+                {item.email && (
+                  <>
+                    <span className='block'>
+                      {tPostItemForm("email")}: {item.email}
+                    </span>
+                  </>
+                )}
+              </p>
+            </div>
+          </div>
+          <div className='flex space-x-2 mt-2 md:mt-0'>
+            {item.status === "pending" ? (
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant='outline'
+                    size='icon'
+                    className='flex items-center justify-center'
+                  >
+                    <BookCheck className='w-4 h-4' />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Publish this item</TooltipContent>
+              </Tooltip>
+            ) : (
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button
+                    variant='outline'
+                    size='icon'
+                    className='flex items-center justify-center'
+                  >
+                    <BookDashed className='w-4 h-4' />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>Unpublish this item</TooltipContent>
+              </Tooltip>
+            )}
+            <EditItemForm
+              itemId={item.id}
+              initialValues={{
+                title: item.title,
+                description: item.description,
+                images: item.images,
+                streetName: item.street_name,
+                streetNumber: item.street_number,
+                city: item.city,
+                postalCode: item.postal_code,
+                country: item.country,
+                phoneNumber: item.phone_number,
+                email: item.email,
+                isHaveWhatsApp: item.is_have_whatsapp,
+              }}
+              translation={{
+                formTitle: tEditItemForm("formTitle"),
+                formDescription: tEditItemForm("formDescription"),
+                title: tPostItemForm("title"),
+                titlePlaceholder: tPostItemForm("titlePlaceholder"),
+                titleError: tPostItemForm("titleError"),
+                description: tPostItemForm("description"),
+                descriptionPlaceholder: tPostItemForm("descriptionPlaceholder"),
+                descriptionError: tPostItemForm("descriptionError"),
+                uploadImages: tPostItemForm("uploadImages"),
+                addressDetails: tPostItemForm("addressDetails"),
+                streetName: tPostItemForm("streetName"),
+                streetNamePlaceholder: tPostItemForm("streetNamePlaceholder"),
+                streetNameError: tPostItemForm("streetNameError"),
+                streetNumber: tPostItemForm("streetNumber"),
+                streetNumberPlaceholder: tPostItemForm(
+                  "streetNumberPlaceholder"
+                ),
+                streetNumberError: tPostItemForm("streetNumberError"),
+                city: tPostItemForm("city"),
+                cityPlaceholder: tPostItemForm("cityPlaceholder"),
+                cityError: tPostItemForm("cityError"),
+                postalCode: tPostItemForm("postalCode"),
+                postalCodePlaceholder: tPostItemForm("postalCodePlaceholder"),
+                postalCodeError: tPostItemForm("postalCodeError"),
+                country: tPostItemForm("country"),
+                contactDetails: tPostItemForm("contactDetails"),
+                contactViaSite: tPostItemForm("contactViaSite"),
+                phoneNumber: tPostItemForm("phoneNumber"),
+                phoneNumberPlaceholder: tPostItemForm("phoneNumberPlaceholder"),
+                phoneNumberError: tPostItemForm("phoneNumberError"),
+                isHaveWhatsApp: tPostItemForm("isHaveWhatsApp"),
+                isHaveWhatsAppTip: tPostItemForm("isHaveWhatsAppTip"),
+                email: tPostItemForm("email"),
+                emailPlaceholder: tPostItemForm("emailPlaceholder"),
+                emailError: tPostItemForm("emailError"),
+                submitButton: tPostItemForm("submitButton"),
+                cancel: tGenericForm("cancel"),
+                required: tGenericForm("required"),
+                reset: tGenericForm("reset"),
+                countries: {
+                  israel: tCountries("il"),
+                  usa: tCountries("usa"),
+                },
+              }}
+            />
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Button
+                  variant='destructive'
+                  size='icon'
+                  onClick={() => deleteItem(item.id)}
+                >
+                  <Trash2 className='w-4 h-4' />
+                </Button>
+              </TooltipTrigger>
+              <TooltipContent>Delete this item</TooltipContent>
+            </Tooltip>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}

87f6e84c921fc62863934c33974a20814c70adc2
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-07 15:47:41 +0300

    feat: replace loading skeleton implementation with ItemsSkeleton component

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,11 +1,5 @@
-import { Skeleton } from "@/components/ui/skeleton";
+import ItemsSkeleton from "@/components/skeletons/itemsSkeleton/ItemsSkeleton";
 
 export default function Loading() {
-  return (
-    <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 gap-4'>
-      {Array.from({ length: 10 }).map((_, i) => (
-        <Skeleton key={i} className='h-64 w-full rounded-2xl' />
-      ))}
-    </div>
-  );
+  return <ItemsSkeleton />;
 }

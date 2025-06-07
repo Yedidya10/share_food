195f1e024fbde442cf37189cb129829cdd3958ac
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-07 15:47:31 +0300

    feat: add ItemsSkeleton component for loading state in item lists

---
+++ src/components/skeletons/itemsSkeleton/ItemsSkeleton.tsx
@@ -0,0 +1,11 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function ItemsSkeleton() {
+  return (
+    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-6 p-4'>
+      {Array.from({ length: 10 }).map((_, i) => (
+        <Skeleton key={i} className='h-64 w-full rounded-2xl' />
+      ))}
+    </div>
+  );
+}

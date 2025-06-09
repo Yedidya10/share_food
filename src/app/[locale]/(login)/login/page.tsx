f4bdb983eb8114519ba37accb857d075e4e2dda5
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-09 17:34:59 +0300

    fix: update redirect handling in LoginModal for improved security and functionality

--- src/app/[locale]/(login)/login/page.tsx
+++ src/app/[locale]/(login)/login/page.tsx
@@ -8,12 +8,17 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { useTranslations } from "next-intl";
-import { useRouter } from "next/navigation";
+import { useRouter } from "@/i18n/navigation";
+import { useSearchParams } from "next/navigation";
 import { useCallback, useEffect } from "react";
 
 export default function LoginModal() {
-  const tLogin = useTranslations("header.login");
+  const searchParams = useSearchParams();
+  const redirect = searchParams.get("redirect") || "/";
+  const safeRedirect = redirect.startsWith("/") ? redirect : "/"; // Fallback to root if redirect is invalid for security
   const router = useRouter();
+
+  const tLogin = useTranslations("header.login");
 
   const handleClose = useCallback(() => {
     router.back();
@@ -29,11 +34,12 @@ export default function LoginModal() {
 
   return (
     <Dialog open onOpenChange={handleClose}>
-      <DialogContent className="sm:max-w-[425px]">
+      <DialogContent className='sm:max-w-[425px]'>
         <DialogHeader>
           <DialogTitle>התחברות</DialogTitle>
         </DialogHeader>
         <ConnectWithGoogle
+          redirectTo={safeRedirect}
           translation={{
             ConnectWithGoogle: tLogin("connectWith", { provider: "Google" }),
           }}

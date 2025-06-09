1488499990d503961ab1ed41ef25305d3393ee20
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-09 17:35:07 +0300

    fix: update ConnectWithGoogle to use locale in sign-in redirect for improved internationalization

--- src/components/ConnectWithGoogle.tsx
+++ src/components/ConnectWithGoogle.tsx
@@ -3,21 +3,22 @@
 import { FaGoogle } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
 import { signInWithOAuth } from "@/lib/supabase/actions";
-import { usePathname } from "next/navigation";
+import { Locale, useLocale } from "next-intl";
 
 export default function ConnectWithGoogle({
+  redirectTo,
   translation,
 }: {
+  redirectTo: string;
   translation: {
     ConnectWithGoogle: string;
   };
 }) {
-  const pathname = usePathname();
+  const locale = useLocale() as Locale;
+  
   return (
     <Button
-      onClick={() =>
-        signInWithOAuth({ provider: "google", redirectTo: pathname })
-      }
+      onClick={() => signInWithOAuth({ provider: "google", redirectTo, locale })}
       className='bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 hover:bg-gray-100 dark:hover:bg-gray-700 border border-gray-300 dark:border-gray-600 w-full flex items-center justify-center cursor-pointer'
     >
       <FaGoogle className='text-gray-900 dark:text-gray-100' />

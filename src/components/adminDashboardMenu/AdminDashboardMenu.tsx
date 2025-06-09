311f2494f27c9cfa78041e459cbd10cc646c7a37
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-09 15:47:17 +0300

    feat: enhance AdminDashboardMenu with icons for navigation items

--- src/components/adminDashboardMenu/AdminDashboardMenu.tsx
+++ src/components/adminDashboardMenu/AdminDashboardMenu.tsx
@@ -1,12 +1,12 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { Link, usePathname } from "@/i18n/navigation";
 import { cn } from "@/lib/utils";
+import { Shapes, Users } from "lucide-react";
 
 const navItems = [
-  { label: "Users", href: "/admin-dashboard/users" },
-  { label: "Items", href: "/admin-dashboard/items" },
+  { label: "Users", icon: <Users />, href: "/admin-dashboard/users" },
+  { label: "Items", icon: <Shapes />, href: "/admin-dashboard/items" },
 ];
 
 export default function AdminDashboardMenu({
@@ -17,7 +17,7 @@ export default function AdminDashboardMenu({
   const pathname = usePathname();
 
   return (
-    <div className='flex flex-col md:flex-row min-h-screen text-gray-900 bg-white dark:bg-gray-900 dark:text-gray-100'>
+    <div className='flex flex-col md:flex-row text-gray-900 bg-white dark:bg-gray-900 dark:text-gray-100 h-[calc(100vh-80px)]'>
       <nav className='w-full md:w-64 bg-gray-100 p-4 border-r border-gray-200 dark:bg-gray-800 dark:border-gray-700'>
         <ul className='space-y-2'>
           {navItems.map((item) => (
@@ -25,11 +25,12 @@ export default function AdminDashboardMenu({
               <Link
                 href={item.href}
                 className={cn(
-                  "block p-2 rounded hover:bg-gray-200 dark:hover:bg-gray-700 transition-colors",
-                  pathname === item.href && "bg-gray-300 font-semibold"
+                  "flex items-center p-2 rounded-md hover:bg-gray-200 dark:hover:bg-gray-700 space-x-4",
+                  pathname === item.href ? "bg-gray-200 dark:bg-gray-700" : ""
                 )}
               >
-                {item.label}
+                {item.icon}
+                <span>{item.label}</span>
               </Link>
             </li>
           ))}

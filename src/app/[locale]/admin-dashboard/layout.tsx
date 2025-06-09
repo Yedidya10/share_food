db61197189032f53c7d7319d194462e273e0590a
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-09 15:43:42 +0300

    feat: enhance admin dashboard layout with user authentication and role validation

--- src/app/[locale]/admin-dashboard/layout.tsx
+++ src/app/[locale]/admin-dashboard/layout.tsx
@@ -1,13 +1,54 @@
 import AdminDashboardMenu from "@/components/adminDashboardMenu/AdminDashboardMenu";
+import { createClient } from "@/lib/supabase/server";
+import { redirect } from "@/i18n/navigation";
 
-export default function DashboardLayout({
+export default async function DashboardLayout({
   children,
+  params,
 }: {
   children: React.ReactNode;
+  params: Promise<{ locale: string }>;
 }) {
-  return (
-    <AdminDashboardMenu>
-      <main className='flex-1 p-4'>{children}</main>
-    </AdminDashboardMenu>
-  );
+  const { locale } = await params;
+
+  try {
+    const supabase = await createClient();
+
+    const { data: userData, error: userError } = await supabase.auth.getUser();
+    if (userError) {
+      throw new Error(userError.message);
+    }
+
+    const { data: userRoles, error: errorRoles } = await supabase
+      .from("user_roles")
+      .select("role")
+      .eq("user_id", userData?.user?.id);
+
+    if (errorRoles) {
+      throw new Error(errorRoles.message);
+    }
+
+    const isAdmin = userRoles?.some((role) => role.role === "admin");
+    if (!isAdmin) {
+      throw new Error("Access denied: Admin role required");
+    }
+
+    return (
+      <>{isAdmin && <AdminDashboardMenu>{children}</AdminDashboardMenu>}</>
+    );
+  } catch (error) {
+    console.error(error);
+    if (
+      error instanceof Error &&
+      error.message === "Access denied: Admin role required"
+    ) {
+      return redirect({ href: "/", locale });
+    }
+
+    if (error instanceof Error && error.message.includes("not authenticated")) {
+      return redirect({ href: "/login", locale });
+    }
+
+    return null;
+  }
 }

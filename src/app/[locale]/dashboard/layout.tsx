0a2dcb2c27c91262448caee983ce2e1655ce5e73
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-10 04:09:37 +0300

    refactor: simplify DashboardLayout component structure by removing unnecessary markup

--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -5,9 +5,5 @@ export default function DashboardLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return (
-    <UserDashboardMenu>
-      <main className='flex-1 p-4'>{children}</main>
-    </UserDashboardMenu>
-  );
+  return <UserDashboardMenu>{children}</UserDashboardMenu>;
 }

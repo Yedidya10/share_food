54ac78f4f65c7d72c9328cd83c495a42b7261545
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-13 02:22:56 +0300

    fix: update routing configuration to support only Hebrew locale

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,8 +2,8 @@ import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["en", "he"],
+  locales: [ "he"],
 
   // Used when no locale matches
-  defaultLocale: "en",
+  defaultLocale: "he",
 });

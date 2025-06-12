b14e61a7e6f7730c2eac9d8ff9ee3e315dedfeea
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-13 02:23:34 +0300

    feat: add item form types for improved type safety and structure

---
+++ src/types/forms/item/item.d.ts
@@ -0,0 +1,59 @@
+type ItemBaseFormValues = {
+  title: string;
+  description: string;
+  streetName: string;
+  streetNumber: string;
+  city: string;
+  country: string;
+  postalCode: string;
+  contactViaSite: boolean;
+  contactByPhone: boolean;
+  contactByEmail: boolean;
+  phoneNumber?: string | null;
+  isHaveWhatsApp?: boolean;
+  email?: string | null;
+};
+
+export type InitialValues = {
+  title: string;
+  description: string;
+  images: string[];
+  streetName: string;
+  streetNumber: string;
+  city: string;
+  country: string;
+  postalCode?: string;
+  contactByPhone: boolean;
+  contactByEmail: boolean;
+  phoneNumber?: string
+  isHaveWhatsApp: boolean;
+  email?: string;
+};
+
+export type dBitem = {
+  id: string;
+  title: string;
+  description: string;
+  images: string[];
+  street_name: string;
+  street_number: string;
+  city: string;
+  postal_code: string;
+  country: string;
+  phone_number: string;
+  is_have_whatsapp: boolean;
+  email: string;
+  status: "published" | "pending" | "draft";
+  user_id: string;
+  created_at: string;
+};
+
+// Extend the default form values with images
+export type PostItemFormValues = ItemBaseFormValues & {
+  images: File[];
+};
+
+export type EditItemFormValues = ItemBaseFormValues & {
+  images: File[] | string[];
+  itemId: string;
+};

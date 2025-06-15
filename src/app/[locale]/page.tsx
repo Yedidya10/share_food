665170aa615b9818f300e692f9292ede4b206c50
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-15 23:20:01 +0300

    feat: integrate user data fetching and update item filtering in Home component

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,6 +4,7 @@ import {
   FoodStatsBanner,
   StatItem,
 } from "@/components/foodSaveBanner/FoodSaveBanner";
+import FilterAndSearchBar from "@/components/productSearchBar/filterAndSearchBar/FilterAndSearchBar";
 
 export default async function Home() {
   const stats: StatItem[] = [
@@ -38,10 +39,16 @@ export default async function Home() {
   ];
 
   const supabase = await createClient();
+  const { data: userData, error: userError } = await supabase.auth.getUser();
+  if (userError) {
+    console.error("Error fetching user data:", userError);
+  }
 
   const { data: itemsData, error: itemsError } = await supabase
     .from("active_items")
     .select("*")
+    .filter("user_id", "neq", userData?.user?.id || "")
+    .filter("status", "eq", "published")
     .order("created_at", { ascending: false });
 
   if (itemsError) {
@@ -57,6 +64,7 @@ export default async function Home() {
 
   return (
     <div className='flex flex-col p-4 gap-4'>
+      <FilterAndSearchBar />
       <FoodStatsBanner stats={stats} />
       <PostGrid items={itemsData || []} />
     </div>

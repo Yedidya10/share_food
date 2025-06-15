653e014502c09e7632af101cfd7943d736061120
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-15 23:26:06 +0300

    feat: add EditItemFormWrapper component for managing item edits

---
+++ src/components/forms/editItemForm/EditItemFormWrapper.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import { InitialValues } from "@/types/forms/item/item";
+import { TranslationType } from "@/types/translation";
+import dynamic from "next/dynamic";
+
+const EditItemForm = dynamic(() => import("./EditItemForm"), {
+  ssr: false,
+});
+
+export default function EditItemFormWrapper(props: {
+  itemId: string;
+  initialValues: InitialValues;
+  translation: TranslationType;
+  open: boolean;
+  onClose: () => void;
+}) {
+  if (!props.open) return null;
+
+  return <EditItemForm {...props} />;
+}

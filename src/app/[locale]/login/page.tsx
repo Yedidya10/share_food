d80ab1359bb9a80a2abde5dd1725e7dd6e0aa2f0
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-17 04:08:35 +0300

    feat: implement login page and form with Google OAuth integration

---
+++ src/app/[locale]/login/page.tsx
@@ -0,0 +1,17 @@
+import { LoginForm } from "@/components/login-form";
+
+export default async function LoginPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ redirect: string }>;
+}) {
+  const { redirect } = await searchParams;
+
+  return (
+    <div className='bg-muted flex min-h-[calc(100svh-80px)] flex-col items-center justify-center gap-6 p-6'>
+      <div className='flex w-full max-w-sm flex-col gap-6'>
+        <LoginForm redirectTo={redirect} />
+      </div>
+    </div>
+  );
+}

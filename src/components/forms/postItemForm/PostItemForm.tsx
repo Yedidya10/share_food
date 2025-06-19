9efddf49bad423f4706b1c8c94aee7a2654762ca
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-19 18:44:15 +0300

    feat: clean up PostItemForm by removing unused console logs and toast notifications

--- src/components/forms/postItemForm/PostItemForm.tsx
+++ src/components/forms/postItemForm/PostItemForm.tsx
@@ -25,7 +25,6 @@ import ImagesFormField from "@/components/forms/ImagesFormField";
 import ItemBaseFormFields from "@/components/forms/ItemBaseFormFields";
 import { TranslationType } from "@/types/translation";
 import { UnifiedImage } from "@/types/forms/item/unifiedImage";
-import { toast } from "sonner";
 import insertItemToDatabase from "./insertItemToDatabase";
 
 export default function PostItemForm({
@@ -40,7 +39,6 @@ export default function PostItemForm({
   translation: TranslationType;
 }) {
   const [images, setImages] = useState<UnifiedImage[]>([]); // Initialize with an empty array
-  console.log("PostItemForm:", images);
   const locale = useLocale();
   // const supabase = createClient();
 
@@ -65,17 +63,12 @@ export default function PostItemForm({
       const response = await insertItemToDatabase(values);
       if (response?.success) {
         setIsSubmitSuccess(true);
-        setOpenModal(false); // Close the modal after successful submission
-        // postItemForm.reset();
-        // setImages([]); // Clear images after successful submission
+        setOpenModal(false);
+        postItemForm.reset();
+        setImages([]); // Clear images after successful submission
       } else {
-        toast.error("שגיאה ביצירת הפריט", {
-          description: response?.message || "אנא נסה שוב מאוחר יותר.",
-          position: "top-center",
-          duration: 5000,
-          richColors: true,
-        });
-        setOpenModal(false); // Close the modal if there was an error
+        setIsSubmitSuccess(false);
+        setOpenModal(false);
         console.error("Error inserting item:", response?.message);
       }
     } catch (error) {
@@ -84,68 +77,57 @@ export default function PostItemForm({
   };
 
   return (
-    <>
-      <Dialog open={openModal} onOpenChange={setOpenModal}>
-        <DialogContent className='sm:max-w-[725px] h-[80vh] overflow-y-auto flex flex-col gap-8'>
-          <DialogHeader className='m-auto'>
-            <DialogTitle
-              className='text-center text-2xl font-semibold'
-              dir={locale === "he" ? "rtl" : "ltr"}
-            >
-              {translation.formTitle}
-            </DialogTitle>
-            <DialogDescription className='text-center text-sm text-muted-foreground'>
-              {translation.formDescription}
-            </DialogDescription>
-          </DialogHeader>
-          <Form {...postItemForm}>
-            <form
-              onSubmit={postItemForm.handleSubmit(onSubmit)}
-              className='space-y-8'
-            >
-              <ItemBaseFormFields
-                form={postItemForm}
-                translation={translation}
-              />
-              <ImagesFormField
-                form={postItemForm}
-                translation={translation}
-                state={{
-                  images,
-                  setImages,
-                }}
-              />
-              <Separator />
-              <LocationFormFields
-                form={postItemForm}
-                translation={translation}
-              />
-              <Separator />
-              <ContactFormFields
-                form={postItemForm}
-                translation={translation}
-              />
-              <DialogFooter>
-                <DialogClose asChild>
-                  <Button type='button' variant='outline'>
-                    {translation.cancel}
-                  </Button>
-                </DialogClose>
-                <Button
-                  type='submit'
-                  disabled={
-                    !postItemForm.formState.isValid ||
-                    postItemForm.formState.disabled ||
-                    postItemForm.formState.isSubmitting
-                  }
-                >
-                  {translation.submitButton}
+    <Dialog open={openModal} onOpenChange={setOpenModal}>
+      <DialogContent className='sm:max-w-[725px] h-[80vh] overflow-y-auto flex flex-col gap-8'>
+        <DialogHeader className='m-auto'>
+          <DialogTitle
+            className='text-center text-2xl font-semibold'
+            dir={locale === "he" ? "rtl" : "ltr"}
+          >
+            {translation.formTitle}
+          </DialogTitle>
+          <DialogDescription className='text-center text-sm text-muted-foreground'>
+            {translation.formDescription}
+          </DialogDescription>
+        </DialogHeader>
+        <Form {...postItemForm}>
+          <form
+            onSubmit={postItemForm.handleSubmit(onSubmit)}
+            className='space-y-8'
+          >
+            <ItemBaseFormFields form={postItemForm} translation={translation} />
+            <ImagesFormField
+              form={postItemForm}
+              translation={translation}
+              state={{
+                images,
+                setImages,
+              }}
+            />
+            <Separator />
+            <LocationFormFields form={postItemForm} translation={translation} />
+            <Separator />
+            <ContactFormFields form={postItemForm} translation={translation} />
+            <DialogFooter>
+              <DialogClose asChild>
+                <Button type='button' variant='outline'>
+                  {translation.cancel}
                 </Button>
-              </DialogFooter>
-            </form>
-          </Form>
-        </DialogContent>
-      </Dialog>
-    </>
+              </DialogClose>
+              <Button
+                type='submit'
+                disabled={
+                  !postItemForm.formState.isValid ||
+                  postItemForm.formState.disabled ||
+                  postItemForm.formState.isSubmitting
+                }
+              >
+                {translation.submitButton}
+              </Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
   );
 }

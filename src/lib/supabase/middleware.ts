cc1fe1328c91dbcbf7c623b1d4b001a4038762e2
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-19 09:41:27 +0300

    feat: enhance session management in middleware for improved authentication flow

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -1,11 +1,10 @@
 import { createServerClient } from "@supabase/ssr";
 import { NextResponse, type NextRequest } from "next/server";
-
-export async function updateSession(request: NextRequest) {
-  let supabaseResponse = NextResponse.next({
-    request,
-  });
-
+import { routing } from "@/i18n/routing";
+export async function updateSession(
+  request: NextRequest,
+  response: NextResponse
+) {
   const supabase = createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
@@ -18,11 +17,8 @@ export async function updateSession(request: NextRequest) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value)
           );
-          supabaseResponse = NextResponse.next({
-            request,
-          });
           cookiesToSet.forEach(({ name, value, options }) =>
-            supabaseResponse.cookies.set(name, value, options)
+            response.cookies.set(name, value, options)
           );
         },
       },
@@ -39,16 +35,51 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
-  if (
-    request.nextUrl.pathname !== "/" &&
-    !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
-  ) {
-    // no user, potentially respond by redirecting the user to the login page
+  const { pathname } = request.nextUrl;
+
+  // Check if the pathname starts with a locale
+  const segments = pathname.split("/");
+  const potentialLocale = segments[1];
+  const isLocale = routing.locales.includes(
+    potentialLocale as (typeof routing.locales)[number]
+  );
+  const pathWithoutLocale = isLocale
+    ? "/" + segments.slice(2).join("/")
+    : pathname;
+
+  // Check if the path is public
+  // (i.e., does not require authentication)
+  const isPublicPath =
+    pathWithoutLocale === "/" ||
+    pathWithoutLocale.startsWith("/auth") ||
+    pathWithoutLocale.startsWith("/login");
+
+  // If the user is not authenticated and the path is not public,
+  // redirect them to the login page.
+  if (!user && !isPublicPath) {
     const url = request.nextUrl.clone();
-    url.pathname = "/auth/login";
+    url.pathname = isLocale ? `/${potentialLocale}/auth/login` : "/auth/login";
+    url.searchParams.set("redirectTo", pathname); // set the redirect path
     return NextResponse.redirect(url);
+  }
+
+  // If the user is authenticated and the path is public,
+  const isAdminPath = pathWithoutLocale.startsWith("/admin-dashboard");
+
+  if (user && isAdminPath) {
+    // Check if the user has the 'admin' role
+    const { data: rolesData, error } = await supabase
+      .from("user_roles")
+      .select("role")
+      .eq("user_id", user.id)
+      .single();
+
+    // If the user does not have the 'admin' role, redirect them to the home page
+    if (error || rolesData?.role !== "admin") {
+      const url = request.nextUrl.clone();
+      url.pathname = isLocale ? `/${potentialLocale}/` : "/";
+      return NextResponse.redirect(url);
+    }
   }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
@@ -64,5 +95,5 @@ export async function updateSession(request: NextRequest) {
   // If this is not done, you may be causing the browser and server to go out
   // of sync and terminate the user's session prematurely!
 
-  return supabaseResponse;
+  return response;
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,21 @@
-import createIntlMiddleware from "next-intl/middleware";
+import createMiddleware from "next-intl/middleware";
 import { updateSession } from "@/lib/supabase/middleware";
 import { type NextRequest } from "next/server";
 import { routing } from "./i18n/routing";
 
-const intlMiddleware = createIntlMiddleware(routing);
+const handleI18nRouting = createMiddleware(routing);
 
-export default async function middleware(request: NextRequest) {
-  // Update the session for Supabase
-  await updateSession(request);
+export async function middleware(request: NextRequest) {
+  const response = handleI18nRouting(request);
 
-  // Handle internationalization
-  return intlMiddleware(request);
+  // A `response` can now be passed here
+  return await updateSession(request, response);
 }
 
 export const config = {
   /*
   // Match all pathnames except:
+  // - … if they end with locale (e.g. `/en`, `/de`)
   // - … if they start with `/api`, `/trpc`, `/_next` or `/_vercel`
   // - … the ones containing a dot (e.g. `favicon.ico`)
      * - _next/static (static files)
@@ -24,5 +24,9 @@ export const config = {
      * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
      * Feel free to modify this pattern to include more paths.
      */
-  matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
+  matcher: [
+    "/",
+    "/(he|en)(/(?!api|trpc|_next|_vercel|.*\\..*).*)?",
+    "/((?!api|trpc|_next|_vercel|.*\\..*).*)",
+  ],
 };

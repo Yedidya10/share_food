2461d82933d7fec4ffac81e45c6e07744936f7e1
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-19 18:39:04 +0300

    feat: update Home component layout and temporarily comment out FoodStatsBanner

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,72 +1,75 @@
 import PostGrid from "@/components/postGrid/PostGrid";
-import { createClient } from "@/lib/supabase/server";
-import {
-  FoodStatsBanner,
-  StatItem,
-} from "@/components/foodSaveBanner/FoodSaveBanner";
-import FilterAndSearchBar from "@/components/productSearchBar/filterAndSearchBar/FilterAndSearchBar";
+import Image from "next/image";
+// import {
+//   FoodStatsBanner,
+//   StatItem,
+// } from "@/components/foodSaveBanner/FoodSaveBanner";
 
-export default async function Home() {
-  const stats: StatItem[] = [
-    {
-      label: "אובדן מזון בישראל",
-      number: "38",
-      unit: "אחוזים",
-      lightColor: "text-red-600",
-      darkColor: "text-red-400",
-    },
-    {
-      label: "שווי אובדן שנתי",
-      number: "24.3",
-      unit: "מיליארד שקלים",
-      lightColor: "text-yellow-600",
-      darkColor: "text-yellow-400",
-    },
-    {
-      label: "היקף אובדן מזון",
-      number: "2.6",
-      unit: "מיליון טון",
-      lightColor: "text-orange-600",
-      darkColor: "text-orange-400",
-    },
-    {
-      label: "כמות מזון בר הצלה",
-      number: "1.2",
-      unit: "מיליון טון",
-      lightColor: "text-green-600",
-      darkColor: "text-green-400",
-    },
-  ];
-
-  const supabase = await createClient();
-  const { data: userData, error: userError } = await supabase.auth.getUser();
-  if (userError) {
-    console.error("Error fetching user data:", userError);
-  }
-
-  const { data: itemsData, error: itemsError } = await supabase
-    .from("active_items")
-    .select("*")
-    .filter("user_id", "neq", userData?.user?.id || "")
-    .filter("status", "eq", "published")
-    .order("created_at", { ascending: false });
-
-  if (itemsError) {
-    console.error("Error fetching items:", itemsError);
-    return (
-      <div className='flex items-center justify-center h-screen'>
-        <p className='text-red-500'>
-          Error fetching items. Please try again later.
-        </p>
-      </div>
-    );
-  }
+export default function Home() {
+  // const stats: StatItem[] = [
+  //   {
+  //     label: "אובדן מזון בישראל",
+  //     number: "38",
+  //     unit: "אחוזים",
+  //     lightColor: "text-red-600",
+  //     darkColor: "text-red-400",
+  //   },
+  //   {
+  //     label: "שווי אובדן שנתי",
+  //     number: "24.3",
+  //     unit: "מיליארד שקלים",
+  //     lightColor: "text-yellow-600",
+  //     darkColor: "text-yellow-400",
+  //   },
+  //   {
+  //     label: "היקף אובדן מזון",
+  //     number: "2.6",
+  //     unit: "מיליון טון",
+  //     lightColor: "text-orange-600",
+  //     darkColor: "text-orange-400",
+  //   },
+  //   {
+  //     label: "כמות מזון בר הצלה",
+  //     number: "1.2",
+  //     unit: "מיליון טון",
+  //     lightColor: "text-green-600",
+  //     darkColor: "text-green-400",
+  //   },
+  // ];
 
   return (
-    <div className='flex flex-col p-4 gap-4'>
-      <FilterAndSearchBar />
-      <FoodStatsBanner stats={stats} />
-      <PostGrid items={itemsData || []} />
+    <div className='flex flex-col'>
+      <div className='p-4'>
+        <div className='w-full flex gap-1 h-[250px] rounded-lg overflow-hidden'>
+          <div className='relative h-[inherit] w-[calc(100%/3)]'>
+            <Image
+              src='/20250616_0830_Superheroes Sharing Food_simple_compose_01jxvkdrrzercsv2kwf5bp64y4.png'
+              alt='Sharing food background'
+              fill
+              className='object-cover object-[100%_50%] md:object-[50%_80%]'
+            />
+          </div>
+          <div className='relative h-[inherit] w-[calc(100%/3)]'>
+            <Image
+              src='/20250616_0830_Superheroes Sharing Food_simple_compose_01jxvkdrs0ea8r4ny4rsw4g6k4.png'
+              alt='Sharing food background'
+              fill
+              className='object-cover object-[10%_0%] md:object-[50%_0%]'
+            />
+          </div>
+          <div className='relative h-[inherit] w-[calc(100%/3)]'>
+            <Image
+              src='/20250616_0830_Superheroes Sharing Food_simple_compose_01jxvkdrs1ev4rpts3y630rfk2.png'
+              alt='Sharing food background'
+              fill
+              className='object-cover object-[30%_0%] md:object-[50%_10%]'
+            />
+          </div>
+        </div>
+      </div>
+      {/* Uncomment the following line to include the FoodStatsBanner component */}
+      {/* <FoodStatsBanner stats={stats} /> */}
+      <PostGrid />
     </div>
   );
 }

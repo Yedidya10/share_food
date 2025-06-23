b693e1294b4f924584c936f48a83b859e443bdc4
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-06-23 21:00:20 +0300

    refactor: language localization improvement

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -3,14 +3,41 @@ import { hasLocale } from "next-intl";
 import { routing } from "./routing";
 
 export default getRequestConfig(async ({ requestLocale }) => {
-  // Typically corresponds to the `[locale]` segment
   const requested = await requestLocale;
   const locale = hasLocale(routing.locales, requested)
     ? requested
     : routing.defaultLocale;
 
+  // Dynamically import locale messages
+  const [common, header, generic, postItem, editItem, countries] =
+    await Promise.all([
+      import(`../../locales/${locale}/common.json`).then((m) => m.default),
+      import(`../../locales/${locale}/header.json`).then((m) => m.default),
+      import(`../../locales/${locale}/form/generic.json`).then(
+        (m) => m.default
+      ),
+      import(`../../locales/${locale}/form/postItem.json`).then(
+        (m) => m.default
+      ),
+      import(`../../locales/${locale}/form/editItem.json`).then(
+        (m) => m.default
+      ),
+      import(`../../locales/${locale}/countries.json`).then((m) => m.default),
+    ]);
+
+  const messages = {
+    common,
+    header,
+    form: {
+      generic,
+      postItem,
+      editItem,
+    },
+    countries,
+  };
+
   return {
     locale,
-    messages: (await import(`../../messages/${locale}.json`)).default,
+    messages,
   };
 });

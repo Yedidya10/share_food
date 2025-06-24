8c935c8f7a7973fcab88875e22ce9e83d8dab78d
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-06-25 00:00:00 +0300

    feat: implement welcome email functionality on user sign-in and remove unused hook

--- src/app/api/send-welcome-oauth/route.ts
+++ src/app/api/send-welcome-oauth/route.ts
@@ -1,36 +1,45 @@
 // pages/api/send-welcome-oauth.ts
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: true,
-  auth: { user: process.env.SMTP_USER, pass: process.env.SMTP_PASS },
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
 });
 
 export async function POST(request: NextRequest) {
-  const { email, html } = await request.json();
-  if (!email || !html) {
-    console.error("Email or HTML content is missing in the request body.");
-    return Response.json(
-      { success: false, message: "Email is required." },
-      { status: 400 }
-    );
-  }
-
   try {
+    const { email, html } = await request.json();
+
+    if (!email || !html) {
+      console.error("Email or HTML content is missing in the request body.");
+      return NextResponse.json(
+        { success: false, message: "Email and HTML are required." },
+        { status: 400 }
+      );
+    }
+
     await transporter.sendMail({
       from: process.env.FROM_EMAIL,
       to: email,
-      subject: "ברוך הבא ל-Share Food!",
+      subject: "ברוך הבא ל-SpareBite!",
       html: html,
     });
-    return Response.json({
+
+    return NextResponse.json({
       success: true,
       message: "Welcome email sent successfully.",
     });
-  } catch (err: unknown) {
+  } catch (err) {
     console.error("Error sending welcome email:", err);
+    return NextResponse.json(
+      { success: false, message: "Failed to send welcome email." },
+      { status: 500 }
+    );
   }
 }

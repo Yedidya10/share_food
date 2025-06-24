203e73d1956865487e887c184f312d09d7ac685e
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-06-24 17:36:06 +0300

    feat: replace ItemsList with DataTable for improved item display and management

--- src/app/[locale]/admin-dashboard/items/page.tsx
+++ src/app/[locale]/admin-dashboard/items/page.tsx
@@ -1,5 +1,6 @@
-import ItemsList from "@/components/itemsList/ItemsList";
+import { DataTable } from "@/components/itemsDataTable/DataTable";
+import { Columns } from "@/components/itemsDataTable/Columns";
 
-export default function ItemsPage() {
-  return <ItemsList />;
+export default function UsersPage() {
+  return <DataTable columns={Columns} />;
 }

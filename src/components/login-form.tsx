e6409f26b1a8b4e85823a2e53ec512b788f2c29a
Yedidya Aberjel <yedidya.abergel@gmail.com>
2025-06-25 10:43:07 +0300

    fix: update translation key for Google connection in LoginForm

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -65,7 +65,7 @@ export async function LoginForm({
                       fill='currentColor'
                     />
                   </svg>
-                  {tLogin("connectWith", { provider: "Google" })}
+                  {tLogin("connect_with", { provider: "Google" })}
                 </Button>
               </div>
               {/* <div className='after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t'>

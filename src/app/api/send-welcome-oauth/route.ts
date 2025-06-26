a3d918dd2b40a748ee781ba89e00c8caa59a962a
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-06-26 17:09:24 +0300

    refactor: update nodemailer transport configuration to use smtpConfig

--- src/app/api/send-welcome-oauth/route.ts
+++ src/app/api/send-welcome-oauth/route.ts
@@ -1,45 +1,46 @@
 // pages/api/send-welcome-oauth.ts
-import { NextRequest, NextResponse } from "next/server";
-import nodemailer from "nodemailer";
+import { NextRequest, NextResponse } from 'next/server'
+import nodemailer from 'nodemailer'
+import { smtpConfig } from '@/lib/envConfig'
 
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST,
-  port: Number(process.env.SMTP_PORT),
+  host: smtpConfig.host,
+  port: smtpConfig.port,
   secure: true,
   auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASSWORD,
+    user: smtpConfig.user,
+    pass: smtpConfig.password,
   },
-});
+})
 
 export async function POST(request: NextRequest) {
   try {
-    const { email, html } = await request.json();
+    const { email, html } = await request.json()
 
     if (!email || !html) {
-      console.error("Email or HTML content is missing in the request body.");
+      console.error('Email or HTML content is missing in the request body.')
       return NextResponse.json(
-        { success: false, message: "Email and HTML are required." },
-        { status: 400 }
-      );
+        { success: false, message: 'Email and HTML are required.' },
+        { status: 400 },
+      )
     }
 
     await transporter.sendMail({
       from: process.env.SMTP_FROM_EMAIL,
       to: email,
-      subject: "ברוך הבא ל-SpareBite!",
+      subject: 'ברוך הבא ל-SpareBite!',
       html: html,
-    });
+    })
 
     return NextResponse.json({
       success: true,
-      message: "Welcome email sent successfully.",
-    });
+      message: 'Welcome email sent successfully.',
+    })
   } catch (err) {
-    console.error("Error sending welcome email:", err);
+    console.error('Error sending welcome email:', err)
     return NextResponse.json(
-      { success: false, message: "Failed to send welcome email." },
-      { status: 500 }
-    );
+      { success: false, message: 'Failed to send welcome email.' },
+      { status: 500 },
+    )
   }
 }

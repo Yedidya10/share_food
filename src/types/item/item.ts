9baefe90dd4af40ee7bc43b1fded2020fa6b7b65
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-07-01 00:59:51 +0300

    fix: remove comment on saveAddress field in PostItemFormValues type

--- src/types/item/item.ts
+++ src/types/item/item.ts
@@ -46,7 +46,7 @@ export type DbFoodItem = DbItem & {
 // Extend the default form values with images
 export type PostItemFormValues = ItemBaseFormValues & {
   images: File[]
-  saveAddress: boolean // Optional, defaults to false
+  saveAddress: boolean
 }
 
 export type EditItemFormValues = ItemBaseFormValues & {

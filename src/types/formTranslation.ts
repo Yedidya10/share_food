315ad3fe6e8708ff75795dba31d453a292da280d
Yedidya Averjel <yedidya.abergel@gmail.com>
2025-07-05 00:33:11 +0300

    refactor: reorganize form translation types for improved clarity and structure

--- src/types/formTranslation.ts
+++ src/types/formTranslation.ts
@@ -1,3 +1,60 @@
+export type FormErrorTranslationType = {
+  maxLengthError: string
+  minLengthError: string
+  requiredError: string
+  notFoundError: string
+  invalidError: string
+}
+
+export type AddressFormTranslationBase = {
+  addressDetails: string
+  streetName: string
+  streetNamePlaceholder: string
+  streetNameError: string
+  streetNumber: string
+  streetNumberPlaceholder: string
+  streetNumberError: string
+  city: string
+  cityPlaceholder: string
+  cityError: string
+  country: string
+  postalCode: string
+  postalCodePlaceholder: string
+  postalCodeError: string
+  saveAddress: string
+  saveAddressTip: string
+  saveAddressError: string
+}
+
+export type PhoneFieldTranslationBase = {
+  phoneNumber: string
+  phoneNumberPlaceholder: string
+  phoneNumberError: string
+  isHaveWhatsApp: string
+  isHaveWhatsAppTip: string
+}
+
+export type EmailFormTranslationBase = {
+  email: string
+  emailPlaceholder: string
+  emailError: string
+}
+
+export type ContactFormTranslationBase = {
+  contactViaSite: string
+  contactByPhone: string
+  contactByEmail: string
+  savePhone: string
+  savePhoneTip: string
+} & PhoneFieldTranslationBase &
+  EmailFormTranslationBase
+
+export type AddressFormTranslationFull = AddressFormTranslationBase &
+  FormErrorTranslationType
+
+export type PhoneFieldTranslationFull = PhoneFieldTranslationBase &
+  FormErrorTranslationType
+
 export type FormTranslationType = {
   formTitle: string
   formDescription: string
@@ -13,34 +70,12 @@ export type FormTranslationType = {
   descriptionMaxLength: string
   uploadImages: string
   uploadImagesError: string
-  addressDetails: string
-  streetName: string
-  streetNamePlaceholder: string
-  streetNameError: string
-  streetNumber: string
-  streetNumberPlaceholder: string
-  streetNumberError: string
-  city: string
-  cityPlaceholder: string
-  cityError: string
-  country: string
-  postalCode: string
-  postalCodePlaceholder: string
-  postalCodeError: string
-  contactDetails: string
-  contactViaSite: string
-  phoneNumber: string
-  phoneNumberPlaceholder: string
-  phoneNumberError: string
-  isHaveWhatsApp: string
-  isHaveWhatsAppTip: string
-  email: string
-  emailPlaceholder: string
-  emailError: string
   submitButton: string
   submitButtonProcessing: string
   cancel: string
   required: string
   reset: string
   israel: string
-}
+} & AddressFormTranslationBase &
+  ContactFormTranslationBase &
+  FormErrorTranslationType
